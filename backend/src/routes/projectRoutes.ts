ee16c669f4abade04dcefe6f20652ebda82239e0
kreonara <kreonara@gmail.com>
2026-09-10 14:45:10 -0600

    Eliminar un Project

--- backend/src/routes/projectRoutes.ts
+++ backend/src/routes/projectRoutes.ts
@@ -37,4 +37,10 @@ router.put('/:id',
   ProjectController.updateProject
 )
 
+router.delete('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  ProjectController.deleteProject
+)
+
 export default router

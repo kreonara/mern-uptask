f5a974757bcabebb3a9ac608631b3589f1dd6c68
kreonara <kreonara@gmail.com>
2026-09-10 00:31:06 -0600

    1er router -> 1er controller

---
+++ backend/src/controllers/ProjectController.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from 'express';
+
+export class ProjectController {
+
+  static getAllProjects(req: Request, res: Response) {
+    res.send('Todos los projectos')
+  }
+}
